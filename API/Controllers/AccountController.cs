7a452dace78d6c36087b19689879333d04a51840
adamkruczkowski <adam.kruczkowsk@gmail.com>
2023-12-21 23:10:32 +0100

    .

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,9 +22,9 @@ namespace API.Controllers
 
         [HttpPost("register")] // api/account/register
 
-        public async Task<ActionResult<UserDto>> Register (RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
             using var hmac = new HMACSHA512();
 
@@ -49,24 +49,26 @@ namespace API.Controllers
 
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x =>
-             x.UserName == loginDto.Username);
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
-            if(user == null) return Unauthorized("invalid username");
+            if (user == null) return Unauthorized("invalid username");
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
 
             var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for(int i = 0; i < computeHash.Length; i++)
+            for (int i = 0; i < computeHash.Length; i++)
             {
-                if(computeHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
             }
 
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
